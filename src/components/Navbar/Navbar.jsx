de2e659f0a8d7e8226371c5da51f123e3505322c
Martin Degraf <martinplate@gmail.comls>
2020-12-02 12:02:27 -0300

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../../assets/moli.svg'
+import './Navbar.css'
+
+function Navbar() { 
+
+    return (
+    <div className="navbar">
+        <img className="logo" src={logo} alt="Logo MOLI SÁNDWICHES DE MIGA Y TORTAS"/>
+        <ul className="menu">
+            <li><a href="home">INICIO</a></li>
+            <li><a href="productos">PRODUCTOS</a></li>
+            <li><a href="catering">CATERING</a></li>
+            <li><a href="contacto">CONTACTO</a></li>
+        </ul>
+    </div>
+)}
+
+export default Navbar
